4ccd6a978ab80d6c622449356aa37402abc0ccac
Churrasqueadas <corporativo@churrasqueadas.com.br>
2026-06-27 14:35:10 -0300

    Marca maior, fundo laranja (classificados/loja)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4205728388" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1829721317" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
