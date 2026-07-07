c1ef80df46b02714bc1415371f038dbfce509c8d
Churrasqueadas <corporativo@churrasqueadas.com.br>
2026-07-06 23:02:14 -0300

    QA: correcoes frontend (bebidas, execucao, busca videos, churrascometro, settings)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1829721317" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3646642355" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
